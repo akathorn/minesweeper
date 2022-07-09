5fd9fb989d827e68e803cd953e0c1ed3a0fa5d1b
Daniel Espino <akathorn@gmail.com>
2022-07-09 07:07:26 +0200

    Add GitHub pages redirection support

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'minesweeper';
+
+  constructor(private router: Router) {
+    // Code to redirect in github pages
+    let redirect = localStorage.getItem('github_redirect');
+    if(redirect) {
+      let path = redirect.split("/").slice(2).join("/")
+      localStorage.removeItem('github_redirect');
+      this.router.navigate([path]);
+    }
+  }
 }
